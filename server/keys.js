f97091aafa84848df54effa8741bff8b342821d6
David Geismar <dageismar@gmail.com>
2018-11-21 12:10:02 +0100

    commit

---
+++ server/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT
+}
